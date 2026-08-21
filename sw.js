676a6377dbb28c9004e23b1a6e2317fb51919e1d
Lior Katz <Liorkatz@gmail.com>
2026-08-21 21:37:02 +0300

    Activate v5 service worker immediately

--- sw.js
+++ sw.js
@@ -1,7 +1,15 @@
-const CACHE = 'al-harosh-v4';
+const CACHE = 'al-harosh-v5';
 const ASSETS = ['./','./index.html','./styles.css','./words.js','./words-more.js','./words-more-a.js','./words-more-b.js','./app.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./icon-180.png'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
-self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(Promise.all([
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))),
+    self.clients.claim()
+  ]));
+});
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
   event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request).then(response => {
