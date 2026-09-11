94a077807f07aea305bf289fb7ca7ee20954be22
Lior Katz <Liorkatz@gmail.com>
2026-09-11 10:21:12 +0300

    Cache Rosh Hashanah deck

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'al-harosh-v9';
-const ASSETS = ['./','./index.html','./styles.css','./difficulty.css','./words.js','./words-more.js','./words-more-a.js','./words-more-b.js','./hard-words.js','./app.js','./sounds.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./icon-180.png'];
+const CACHE = 'al-harosh-v10';
+const ASSETS = ['./','./index.html','./styles.css','./difficulty.css','./words.js','./words-more.js','./words-more-a.js','./words-more-b.js','./hard-words.js','./rosh-hashanah.js','./app.js','./sounds.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./icon-180.png'];
 self.addEventListener('install', event => {
   self.skipWaiting();
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
